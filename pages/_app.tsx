15e47581d118a5c315f91bb66e81fd47e0213ba3
albatros <albert.kolakowski@gmail.com>
2022-11-27 10:32:17 +0100

    Update fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,6 +30,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Blog with shop</title>
         <meta name="description" content="Blog and shop" />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.95em%22 font-size=%2280%22>🐱</text></svg>" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" /*crossorigin*/ />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet"></link>
       </Head>
       <ThemeProvider theme={theme}>
         {/* <ShopContextProvider> */}
